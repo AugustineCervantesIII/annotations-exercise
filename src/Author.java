0e837181a3f4d3014b91ca36a6af46e4b4007f13
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-06-19 12:09:37 -0500

    adding walkthrough code

--- src/Author.java
+++ src/Author.java
@@ -6,28 +6,28 @@ import java.util.List;
 
 @SuppressWarnings("unchecked")
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
-    @SuppressWarnings("unchecked")
+    //@SuppressWarnings("unchecked")
     public List<String> getBooks() {
         return books;
     }
 
-    @SuppressWarnings("deprecated")
+    //@SuppressWarnings("deprecated")
     public List<String> publishedBooks() {
         return books;
     }
 
-    @SuppressWarnings("unchecked")
+    //@SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
